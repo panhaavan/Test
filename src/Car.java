4ed212335b1e62b26c7075fe302788b4ab4ae28f
MR PANHA <151159893+panhaavan@users.noreply.github.com>
2024-01-09 23:44:57 +0700

    java

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+
+//create a class
+public class Car {
+	//data member
+	
+	String make = "Chevrolet";
+	String model = "Corvette";
+	int year = 2024;
+	String color = "blue";
+	double price = 5000.00;
+	
+	//function member(method)
+	
+	void drive() {
+		System.out.println("You drive the car.");
+	}
+	void brake() {
+		System.out.println("You step on the brakes");
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//create an object
+		Car mycar = new Car();
+		
+		
+		//use object
+		System.out.println(mycar.model);
+
+	}
+
+}
